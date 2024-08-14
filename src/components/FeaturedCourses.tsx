3e087a0bf24155b6c7df2e064ce09a6848876c90
Ni2L <nituldas8753@gmail.com>
2024-08-14 01:37:47 -0700

    Update FeaturedCourses.tsx

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -19,7 +19,7 @@ function FeaturedCourses() {
 
 
   return (
-    <div className="py-12 h-[80rem]">
+    <div className="py-12 bg-black/[0.96] ">
         <div>
             <div className="text-center">
                 <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED COURSES</h2>
