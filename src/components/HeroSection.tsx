bdccc6044362838323766a088b794c252e16a244
Ni2L <nituldas8753@gmail.com>
2024-08-14 01:16:08 -0700

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,7 +9,7 @@ function HeroSection() {
       <div className="h-screen md:h-[60rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 px-8">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-40"
-        fill="#cyan"
+        fill="#00FFFF"
       />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
